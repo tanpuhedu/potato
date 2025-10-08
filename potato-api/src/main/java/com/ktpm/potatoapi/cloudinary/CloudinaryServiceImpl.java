b2d1b5313beb24c97afc0d1bfa54ba27e31db44f
tanpuhedu <3122411191@sv.sgu.edu.vn>
2025-10-08 15:24:38 +0700

    #3 - integrate cloudinary

---
+++ potato-api/src/main/java/com/ktpm/potatoapi/cloudinary/CloudinaryServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ktpm.potatoapi.cloudinary;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CloudinaryServiceImpl implements CloudinaryService {
+    Cloudinary cloudinary;
+
+    @Override
+    public String upload(MultipartFile file, String folderName) {
+        try {
+            Map uploadedFile = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.asMap(
+                            "folder", folderName,
+                            "overwrite", true
+                    )
+            );
+            String publicId = uploadedFile.get("public_id").toString();
+            return cloudinary.url().secure(true).generate(publicId);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+}
