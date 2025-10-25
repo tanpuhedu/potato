0e886e10315528d6be0cb39ca82aa566323be55c
aressofaugust98 <tuankiet9804@gmail.com>
2025-10-25 20:53:06 +0700

    Revert "Frontend"

--- customer_fe/src/context/StoreContext.jsx
+++ customer_fe/src/context/StoreContext.jsx
@@ -1,20 +1,16 @@
 import { createContext } from "react";
 import { food_list } from "../assets/assets";
 
-// Tạo context
-export const StoreContext = createContext(null);
+export const StoreContext =createContext(null)
 
 const StoreContextProvide = (props) => {
-  const contextValue = {
-    food_list,
-  };
-
-  return (
-    <StoreContext.Provider value={contextValue}>
-      {props.children}
-    </StoreContext.Provider>
-  );
-};
-
-// Xuất default để import ở file main.jsx dùng được
-export default StoreContextProvide;
+    const contextValue ={
+        food_list
+    }
+    return
+    (
+        <StoreContextProvide value ={contextValue}>
+            {props.children}
+        </StoreContextProvide>
+    )
+}
