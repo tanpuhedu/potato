a73fd1e04a939335622336532d4cd991e8e9d462
tanpuhedu <3122411191@sv.sgu.edu.vn>
2025-11-01 13:37:27 +0700

    #84 - return category in list of menu item responses

--- potato-api/src/main/java/com/ktpm/potatoapi/menu/mapper/MenuItemMapper.java
+++ potato-api/src/main/java/com/ktpm/potatoapi/menu/mapper/MenuItemMapper.java
@@ -5,10 +5,11 @@ import com.ktpm.potatoapi.menu.dto.MenuItemDetailResponse;
 import com.ktpm.potatoapi.menu.dto.MenuItemRequest;
 import com.ktpm.potatoapi.menu.dto.MenuItemResponse;
 import com.ktpm.potatoapi.menu.entity.MenuItem;
+import com.ktpm.potatoapi.option.mapper.OptionMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring", uses = CategoryMapper.class)
+@Mapper(componentModel = "spring", uses = {CategoryMapper.class, OptionMapper.class})
 public interface MenuItemMapper {
     MenuItem toEntity(MenuItemRequest request);
     MenuItemResponse toMenuItemResponse(MenuItem menuItem);
