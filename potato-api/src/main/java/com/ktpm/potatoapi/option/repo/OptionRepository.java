07727771b326f4ef6561f914cdf36d2a2924e17a
tanpuhedu <3122411191@sv.sgu.edu.vn>
2025-11-01 18:52:51 +0700

    #89 - refactor func deleteMenuItem

--- potato-api/src/main/java/com/ktpm/potatoapi/option/repo/OptionRepository.java
+++ potato-api/src/main/java/com/ktpm/potatoapi/option/repo/OptionRepository.java
@@ -1,8 +1,11 @@
 package com.ktpm.potatoapi.option.repo;
 
-import com.ktpm.potatoapi.menu.entity.MenuItem;
 import com.ktpm.potatoapi.option.entity.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +13,9 @@ import java.util.Optional;
 public interface OptionRepository extends JpaRepository<Option, Long> {
     List<Option> findAllByMerchantIdAndIsActiveTrue(Long merchantId);
     Optional<Option> findByIdAndIsActiveTrue(Long optionId);
-    List<Option> findAllByMenuItemsContaining(MenuItem menuItem);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM option_menu_items WHERE menu_item_id = :menuItemId", nativeQuery = true)
+    void deleteAllOptionLinksByMenuItemId(@Param("menuItemId") Long menuItemId);
 }
